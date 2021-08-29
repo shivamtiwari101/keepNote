7df8a8e071e219c69c5d24812b5e61962b8ceb85
shivamtiwari101 <77287982+shivamtiwari101@users.noreply.github.com>
2021-08-29 22:23:59 +0530

    Updates

---
+++ precache-manifest.3723e835ce45231f2e9db65c70392723.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703ca21c64e5268ddfc9a2de78e7ffb4",
+    "url": "/keepNote/index.html"
+  },
+  {
+    "revision": "2bcc2d4a3040950dbe78",
+    "url": "/keepNote/static/js/2.d0696145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keepNote/static/js/2.d0696145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ccd89aa1ec63924a50",
+    "url": "/keepNote/static/js/main.a29823db.chunk.js"
+  },
+  {
+    "revision": "46c86f94cde0c1f2b64e",
+    "url": "/keepNote/static/js/runtime-main.a0e1b1b0.js"
+  }
+]);
